e2d34f90588e8f61af20eee7d0a6e88ee14f1cfa
Tim Simms <tsimms@gmail.com>
2021-09-27 09:22:59 -0700

    betty comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-/**using puts to print to output*/
-int main(void) /**test */
+/**
+ * using puts to print to output
+ * */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
