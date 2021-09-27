e2faa0af5911587e92a130503c1100841b94977f
Tim Simms <tsimms@gmail.com>
2021-09-27 11:09:46 -0700

    push 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * printf: print to file.
+ *
+ * Return: Always 0 (For success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
