fd09e431fdf0004ae2f315208313909065bbc5cd
Tim Simms <tsimms@gmail.com>
2021-09-28 11:29:38 -0700

    push of 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ABC;
+for (ABC = 'a'; ABC <= 'z'; ABC++)
+	putchar (ABC);
+	putchar ('\n');
+return (0);
+}
