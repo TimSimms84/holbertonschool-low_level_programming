c0dfbac006a49532f4642571c67f13346e441658
Tim Simms <tsimms@gmail.com>
2021-09-29 11:36:08 -0700

    push 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Description: Prints the alphabet with _putchar
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+	for (i = 0; i <= 10; i++)
+	{
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+	{
+	_putchar(alpha);
+	}
+	_putchar('\n');
+	}
+}
