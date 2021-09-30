33b30714c4561bbfee3f26ded1a39f28e3e890fc
Tim Simms <tsimms@gmail.com>
2021-09-29 17:32:53 -0700

    push 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ *
+ * Description: Prints the alphabet with _putchar
+ * @i: variable to check
+ * Return: void
+ */
+
+int print_last_digit(int i)
+{
+	i = i % 10;
+	if (i < 0)
+	{
+		i = -i;
+	_putchar('0' + i);
+}
+return (0);
+}
