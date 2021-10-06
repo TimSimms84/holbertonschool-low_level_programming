7d6d21101d251388056b19843e1163599bf86324
Tim <tsimms@gmail.com>
2021-10-06 09:10:06 -0500

    push 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ *
+ * @str: Point to the string
+ *
+ *
+ *
+ *
+ * Return: length of string
+ */
+
+void _puts(char *str)
+{
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+		str++;
+	}
+	_putchar('\n');
+}
