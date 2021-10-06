4f98eb123f249daa5822e56dc8fbd56a0331e34c
Tim <tsimms@gmail.com>
2021-10-06 14:08:22 -0500

    push 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,13 +18,13 @@ void puts_half(char *str)
 	while (str[i] != '\0')
 		i++;
 	if (i % 2 == 0)
-		j = i / 2
-	else 
+		j = i / 2;
+	else
 		j = (i + 1) / 2;
 	while (j < i)
 	{
 		_putchar(str[j]);
 		j++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
