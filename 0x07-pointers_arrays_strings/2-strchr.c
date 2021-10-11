c2fed575431c9e27cdc0f7336570cecef9781e2d
Tim <tsimms@gmail.com>
2021-10-11 14:32:07 -0500

    fix of 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
  * _strchr - checks if a char is in string
  *
  * @s: string to check
  * @c: character to search for
  *
- * Return: Pointer to dest
+ * Return: Pointer fo first char in string if found
+ * else return NULL
  */
 
 char *_strchr(char *s, char c)
@@ -14,8 +14,13 @@ char *_strchr(char *s, char c)
 
 	while (*s != '\0')
 	{
-		if (*s == c)
+		if (*s != c)
+			s++;
+		else
 			return (s);
 	}
+	if (*s == '\0')
+		return (s);
+
 return (0);
 }
