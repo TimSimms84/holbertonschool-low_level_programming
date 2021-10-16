3847ade936f2bcc6d72d21be2be42558c7b93d20
Tim <tsimms@gmail.com>
2021-10-16 15:32:19 -0500

    header guards

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _isupper(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -39,3 +42,5 @@ int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _atoi(char *n);
+
+#endif /* _MAIN_H_ */I
