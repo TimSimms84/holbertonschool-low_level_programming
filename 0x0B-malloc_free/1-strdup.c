e7abe07564a46add2c627cee23148f230a4f0789
Tim <tsimms@gmail.com>
2021-10-20 15:14:30 -0500

    push 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - return ptr cotaining copy of string
+ * @str: string to copy
+ *
+ * Return: pntr to string or NULL if insufficent memory
+ *
+ *
+ */
+
+
+char *_strdup(char *str)
+{
+
+	char *ptr;
+	unsigned int n, strlen = 0;
+
+	while (str[strlen])
+	{
+		strlen++;
+	}
+		strlen++;
+	ptr = malloc(strlen * sizeof(char));
+
+	for (n = 0; n < strlen; n++)
+	{
+		ptr[n] = str[n];
+	}
+
+
+
+return (ptr);
+}
