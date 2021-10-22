e6ea10780bd0ad4eff7a0fe24ffdf60645a48e4e
Tim Simms <tsimms@gmail.com>
2021-10-22 12:29:57 -0700

    repush 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,40 +1,42 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 /**
- * **alloc_grid - 2d array of integers
- * @width: width of grid
- * @height: height of grid
- * Return: pointer to new string
+ *alloc_grid - prints a grid of integers
+ *@width: width of the grid
+ *@height: height of the grid
+ *Return: nothing else
  */
-
 int **alloc_grid(int width, int height)
-
 {
 	int **array;
-	int a, b;
-	/*if 0 or negative return null */
-	if (width < 1 || height < 1)
-		return (NULL);
+	int i;
 
-	array = (int **)malloc(sizeof(int) * height);
+	array = (int **)malloc(height * sizeof(int *));
 
-	for (a = 0 ; a < height ; a++)
+	if (height <= 0 || width <= 0)
 	{
-		array[a] = malloc(sizeof(int) * width);
-		if (array[a] == NULL)
+		return (NULL);
+	}
+	if (array == 0)
+	{
+		free(array);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+	array[i] = (int *)malloc(width * sizeof(int));
+		if (array[i] == NULL)
 		{
-			for (a = a - 1; a >= 0; a--)
+			for (i = 0; i < height; i++)
 			{
-				free(array[a]);
+				free(array[i]);
 			}
 			free(array);
 			return (NULL);
 		}
-		for (b = 0; b < width; b++)
-		{
-			array[a][b] = 0;
-		}
 	}
-	return (array);
+
+return (array);
+
 }
