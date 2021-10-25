b254f9a2debf98a3bff91735b2d5469b56b2dfd7
Tim <tsimms@gmail.com>
2021-10-25 15:41:54 -0500

    fix betty styling

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,30 +3,29 @@
 #include <stdlib.h>
 
 /**
- * _calloc - allocates memory for an array of nmemb elements of 
+ * _calloc - allocates memory for an array of nmemb elements of
  * "size" bytes each and returns a pointer to the allocated memory.
  * @nmemb:number of elements
  * @size: byes in element
- * 
  * Return: pointer to memory
  */
 
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *ptr;
-    unsigned int i;
+	char *ptr;
+	unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    ptr = malloc(nmemb * size);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
 
-    if (!ptr)
-        return (NULL);
+	if (!ptr)
+		return (NULL);
 
-    for (i = 0; i < nmemb * size; i++)
-    {
-        ptr[i] = 0;
-    }
-return (ptr);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
 }
