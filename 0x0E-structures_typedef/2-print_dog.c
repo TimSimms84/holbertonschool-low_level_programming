401043f133d5bc9922c41af0faa2ae946e3839df
Tim <tsimms@gmail.com>
2021-10-26 14:12:34 -0500

    betty issues

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,11 +3,8 @@
 #include <stdlib.h>
 
 /**
- * init_dog - intialize variables in the dog struct
+ * print_dog - print the dog struct
  * @d: pointer to variables
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
  * Return: void
  */
 
@@ -15,17 +12,17 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	
+
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age <1)
+	if (d->age <= 0)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	else	
+	else
 		printf("Owner: %s\n", d->owner);
 }
