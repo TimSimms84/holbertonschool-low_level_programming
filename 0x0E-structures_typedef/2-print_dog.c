9b06df936d1998a61a98b9e625569dae2999729c
Tim <tsimms@gmail.com>
2021-10-26 14:52:07 -0500

    retry 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,19 +10,16 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		exit(1);
+	if (!d)
+		return;
 
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age == '\0')
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+		d->name = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		d->owner = "(nil)";
+	printf("Owner: %s\n", d->owner);
 }
