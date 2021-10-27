81749af05f05240da950ddc861f1710ea1f68357
Tim <tsimms@gmail.com>
2021-10-27 12:36:28 -0500

    init repo

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ *
+ * @name: name to rpint
+ * @f: pointer to function that prints
+ *
+ * Return: void
+ */
+
+
+void print_name(char *name, void (*f)(char *name))
+{
+	f(name);
+}
