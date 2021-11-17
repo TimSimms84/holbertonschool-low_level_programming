713ff1a23fe4ae46ae5b33fcc992565a84293a97
Tim <tsimms@gmail.com>
2021-11-17 16:18:05 -0600

    text content null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
 	fo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
