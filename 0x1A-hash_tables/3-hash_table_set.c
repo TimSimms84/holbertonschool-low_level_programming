15aa27f2a1fb66fba39139d3250d688cc19513ff
Tim <tsimms@gmail.com>
2021-12-11 18:57:44 -0600

    add free in loop

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *search, *new_node = NULL;
 	unsigned long int index;
+	char * new_value;
 
 	if (!ht || !key || !value)
 		return (0);
@@ -25,7 +26,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(search->key, key) == 0)
 		{
-			search->value = strdup(value);
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(search->value);
+			search->value = new_value;
 			return (1);
 		}
 		search = search->next;
