6922f322acb972f30d639c9a37883d683f7baec4
Tim Simms <tsimms@gmail.com>
2022-06-05 15:47:11 -0500

    updated header

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +13,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 int _atoi(char *s);
+
+#endif /* MAIN_H */
