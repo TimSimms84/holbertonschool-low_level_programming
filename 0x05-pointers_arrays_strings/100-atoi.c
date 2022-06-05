3511297e884a2ed36602450271985288d6cf91b4
Tim Simms <tsimms@gmail.com>
2022-06-05 15:43:46 -0500

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+/**
+ * _atoi - convert string to int
+ * @s: passed string
+ * Return: value of string
+ */
+
+int _atoi(char *s)
+{
+	int res = 0;
+	int i = 0;
+	int flag = 0;
+
+
+	for (i; s[i] != '\0'; ++i)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + s[i] - '0';
+		}
+		if (s[i] == '-')
+		{
+			++flag;
+		}
+		if ((s[i] < '0' || s[i] > '9') && res != 0)
+		{
+			break;
+		}
+	}
+
+	if (flag % 2 == 0)
+		flag = 1;
+	else
+		flag = -1;
+
+	return (res * flag);
+}
