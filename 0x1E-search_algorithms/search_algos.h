ae9cc0d552b8f153bd6d554fbff77977436bafdd
Tim Simms <tsimms@gmail.com>
2022-06-13 14:41:02 -0500

    linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* include libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* function declrations */
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS */
