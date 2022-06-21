1ac4c7663b18663f4d0545a071413f1a5d511b84
Tim Simms <tsimms@gmail.com>
2022-06-21 16:43:54 -0500

    recursive binary

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,67 @@
+#include "search_algos.h"
+
+/**
+ * arrayPrinter - prints an array
+ * @start: start of array
+ * @end: end of array
+ * @array: pointer to array
+ */
+void arrayPrinter(int *array, int start, int end)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i < end; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[end]);
+}
+
+/**
+ * recursive_binary - binary search, but recursively
+ * @arr: the array to search
+ * @value: value to find
+ * @left: left of array
+ * @right: right of array
+ * @found: holder to find value
+ * Return: index of value where it was found or -1
+ */
+
+int recursive_binary(int *arr, int value, size_t left, size_t right,
+ssize_t *found)
+{
+	size_t i = left, mid;
+
+	if (left > right)
+		return (*found);
+	arrayPrinter(arr, left, right);
+	mid = ((right - left) / 2) + left;
+	if (arr[mid] == value)
+	{
+		*found = mid;
+		if (right - left > 1)
+			mid++;
+	}
+	if (arr[mid] < value)
+		return (recursive_binary(arr, value, mid + 1, right, found));
+	if (arr[mid] > value)
+		return (recursive_binary(arr, value, left, mid - 1, found));
+	return (*found);
+}
+
+/**
+ * advanced_binary - search, but in binary
+ * @array: the array
+ * @size: size of the array
+ * @value: what to find
+ * Return: index where it is or -1 if not present
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+
+	ssize_t found = -1;
+
+	if (!array)
+		return ((int)found);
+	return (recursive_binary(array, value, 0, size - 1, &found));
+}
