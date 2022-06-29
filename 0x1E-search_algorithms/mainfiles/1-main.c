4a7580a088f95371b724a4997804ad18027e1f96
Tim Simms <tsimms@gmail.com>
2022-06-29 16:45:00 -0500

    main files

--- 0x1E-search_algorithms/mainfiles/1-main.c
+++ 0x1E-search_algorithms/mainfiles/1-main.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
  * main - Entry point
  *
- * Return: Always EXIT_SUCCESS
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    };
-	int array2[17] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 25, 48, 89
+	int array[17] = {
+		3, 4, 5, 7, 7, 7, 8, 9, 10, 11, 21, 31, 33, 33, 33, 44, 51
 	};
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
-    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
-    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
-	printf("Found at index: %d\n", binary_search(array2, 16, 12));
-    return (EXIT_SUCCESS);
+	printf("Found at index: %d\n", advanced_binary(array, 17, 7));
+	return (0);
 }
